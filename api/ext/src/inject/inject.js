1e3f7c1d50afbc5a3e5486729fbd3bf127735710
Ryan Efendy <ryanefendy95@gmail.com>
2017-02-26 10:28:28 -0500

    working on ajax

--- api/ext/src/inject/inject.js
+++ api/ext/src/inject/inject.js
@@ -13,6 +13,8 @@ chrome.runtime.sendMessage({}, function(response) {
             a.setAttribute("data-html", "true");
             a.setAttribute("width", "500px");
 
+            var script = document.createElement("script");
+
             var form =
             '<style>'+
                 '.popover {'+
@@ -21,28 +23,28 @@ chrome.runtime.sendMessage({}, function(response) {
                 '}'+
             '</style>'+
 
-            '<form class="form-horizontal">' +
+            '<form id="newscred-form">' +
             '<fieldset>' +
             '<div class="form-group">' +
               '<div class="col-md-4" style="width: 100%"> ' +
                 '<label class="radio-inline" for="radios-0">' +
-                  '<input type="radio" name="radios" id="radios-0" value="1">' +
+                  '<input type="radio" name="art-score" id="radios-0" value="1">' +
                   '1' +
                 '</label> ' +
                 '<label class="radio-inline" for="radios-1">' +
-                  '<input type="radio" name="radios" id="radios-1" value="2">' +
+                  '<input type="radio" name="art-score" id="radios-1" value="2">' +
                   '2' +
                 '</label> ' +
                 '<label class="radio-inline" for="radios-2">'+
-                  '<input type="radio" name="radios" id="radios-2" value="3">'+
+                  '<input type="radio" name="art-score" id="radios-2" value="3">'+
                   '3' +
                 '</label>' +
                 '<label class="radio-inline" for="radios-3">'+
-                  '<input type="radio" name="radios" id="radios-3" value="4">'+
+                  '<input type="radio" name="art-score" id="radios-3" value="4">'+
                   '4' +
                 '</label>'+
                 '<label class="radio-inline" for="radios-4">'+
-                  '<input type="radio" name="radios" id="radios-4" value="5" checked="checked">'+
+                  '<input type="radio" name="art-score" id="radios-4" value="5" checked="checked">'+
                   '5' +
                 '</label>'+
               '</div>'+
@@ -50,45 +52,44 @@ chrome.runtime.sendMessage({}, function(response) {
 
             '<div class="form-group" style="width: 100%">' +
               '<div class="col-md-12">'+
-                '<textarea class="form-control" id="textarea" name="textarea" placeholder="optional comment..."></textarea>'+
+                '<textarea class="form-control" id="comment-field" name="textarea" placeholder="optional comment..."></textarea>'+
               '</div>'+
             '</div>'+
 
             '<div class="form-group" style="width: 100%">'+
               '<div class="col-md-8">'+
-                '<button id="button1id" name="button1id" class="btn btn-success">Submit</button>'+
+                '<input type="submit" id="button1id" name="button1id" class="btn btn-success">Submit</input>'+
               '</div>'+
             '</div>'+
 
             '</fieldset>'+
-            '</form>' +
+            '</form>'+
+            '<script>'+
+                '$("#newscred-form").submit(function(e) {'+
+                    'console.log($("#comment-field").value);'+
+                    'console.log($("input[name=&quot;art-score]:checked").value);'+
+                    'e.preventDefault();'+
+                '});'+
+            '</script>';
 
-            // $("#idForm").submit(function(e) {
-
-            //     var url = "path/to/your/script.php"; // the script where you handle the form input.
-
-            //     $.ajax({
-            //            type: "POST",
-            //            url: url,
-            //            data: $("#idForm").serialize(), // serializes the form's elements.
-            //            success: function(data)
-            //            {
-            //                alert(data); // show response from the php script.
-            //            }
-            //          });
-
-            //     e.preventDefault(); // avoid to execute the actual submit of the form.
-            // });
+            // var form2 =   '<form>' +
+            //     '<input type="radio" name="foo" value="1" checked="checked" />' +
+            //     '<input type="radio" name="foo" value="0" />' +
+            //     '<input name="bar" value="xxx" />' +
+            //     '<select name="this">' +
+            //     '<option value="hi" selected="selected">Hi</option>' +
+            //     'option value="ho">Ho</option>' +
+            //     '</select>' +
+            //     '</form>';
 
             a.setAttribute("data-content", form);
             a.innerText = "Click Me!";
             popover.append(a);
 
             return popover;
-        }
+        };
 
         try {
-
             var metaAuthor = document.head.querySelector("[name=author]");
             if (metaAuthor.content != undefined) {
                 metaAuthor = metaAuthor.content;
@@ -100,7 +101,7 @@ chrome.runtime.sendMessage({}, function(response) {
             authorBody.parentElement.append(makePopover());
             $('[data-toggle="popover"]').popover({ container: 'body'});
 
-
+            console.log(JSON.stringify($('.form-horizontal').serializeArray()));
         } catch (e) {
 
             console.log("No author found");
