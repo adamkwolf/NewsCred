30492e7877b6eab59f4ce973240e6a3153dc1a82
Adam Wolf <adamwolf@n3-76-76.dhcp.drexel.edu>
2017-02-26 00:09:29 -0500

    inject updates

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -61,7 +61,24 @@ chrome.runtime.sendMessage({}, function(response) {
             '</div>'+
 
             '</fieldset>'+
-            '</form>'
+            '</form>' +
+
+            // $("#idForm").submit(function(e) {
+
+            //     var url = "path/to/your/script.php"; // the script where you handle the form input.
+
+            //     $.ajax({
+            //            type: "POST",
+            //            url: url,
+            //            data: $("#idForm").serialize(), // serializes the form's elements.
+            //            success: function(data)
+            //            {
+            //                alert(data); // show response from the php script.
+            //            }
+            //          });
+
+            //     e.preventDefault(); // avoid to execute the actual submit of the form.
+            // });
 
             a.setAttribute("data-content", form);
             a.innerText = "Click Me!";
