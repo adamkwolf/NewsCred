3ae8b94839c82ca46328dc9951a4e6bf50f0a963
Ryan Efendy <ryanefendy95@gmail.com>
2017-02-26 12:25:53 -0500

    add table & format

--- web/js/angular.js
+++ web/js/angular.js
@@ -45,9 +45,112 @@ searchctrl = newsApp.controller('SearchController', ['$scope', '$http', '$routeP
     }).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-        console.log("done")
-        console.log(response.data)
-        $scope.greeting = response.data
+        // obj to append data to
+        var div = document.getElementById("results");
+        var cardtitle = document.getElementById("card-title");
+
+        var table = document.createElement("table");
+        var tr = document.createElement("tr");
+        var th = document.createElement("th");
+        var node = document.createTextNode("Author");
+        th.appendChild(node);
+        tr.appendChild(th);
+        table.appendChild(tr);
+
+        th = document.createElement("th");
+        node = document.createTextNode("Article");
+        th.appendChild(node);
+        tr.appendChild(th);
+        table.appendChild(tr);
+
+        th = document.createElement("th");
+        node = document.createTextNode("Company");
+        th.appendChild(node);
+        tr.appendChild(th);
+        table.appendChild(tr);
+
+        th = document.createElement("th");
+        node = document.createTextNode("Url");
+        th.appendChild(node);
+        tr.appendChild(th);
+        table.appendChild(tr);
+
+        th = document.createElement("th");
+        node = document.createTextNode("Rating");
+        th.appendChild(node);
+        tr.appendChild(th);
+        table.appendChild(tr);
+
+        div.appendChild(table);
+
+        // result object
+        var result = response.data.result;
+        cardtitle.innerHTML = "Search Results: " + parseInt(result.articles.length);
+
+        console.log(result);
+
+        // author
+        var author = result.author.name;
+
+        var rating = result.rating;
+
+        // div.append(author);
+
+        // individual article data
+        for (var i = 0; i < result.articles.length; i++) {
+            var company = result.articles[i].site.company;
+            var website = result.articles[i].site.website_url;
+            var snapshot = result.articles[i].snapshot;
+            var title = result.articles[i].title;
+            var url = result.articles[i].url;
+            // div.append(company);
+            //
+            // div.append(website);
+            // div.append(title);
+            // div.append(url);
+
+            var tr = document.createElement("tr");
+            var th = document.createElement("th");
+            var node = document.createTextNode(author);
+            th.appendChild(node);
+            tr.appendChild(th);
+            table.appendChild(tr);
+
+            th = document.createElement("th");
+            node = document.createTextNode(title);
+            th.appendChild(node);
+            tr.appendChild(th);
+            table.appendChild(tr);
+
+            th = document.createElement("th");
+            node = document.createTextNode(company);
+            th.appendChild(node);
+            tr.appendChild(th);
+            table.appendChild(tr);
+
+            th = document.createElement("th");
+            node = document.createTextNode(url);
+            th.appendChild(node);
+            tr.appendChild(th);
+            table.appendChild(tr);
+
+            th = document.createElement("th");
+            node = document.createTextNode(rating);
+            th.appendChild(node);
+            tr.appendChild(th);
+            table.appendChild(tr);
+        }
+
+        function addStyleString(str) {
+            var node = document.createElement('style');
+            node.innerHTML = str;
+            document.body.appendChild(node);
+        }
+
+        addStyleString('table { font-family: arial, sans-serif; border-collapse: collapse; width: 100%; }');
+        addStyleString('td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; }');
+
+        console.log(table);
 
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
